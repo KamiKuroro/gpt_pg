f95d80b2b4a06a9f2728f65ae4616c68404ad985
wangxinyi <wangxinyi@tsingj.com>
2023-03-26 21:28:17 +0800

    upt

--- gpt-frontend/src/App.vue
+++ gpt-frontend/src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <div id="app">
-    <input v-model="userInput" @keyup.enter="sendMessage" placeholder="Type your message..." />
-    <button @click="sendMessage">Send</button>
+    <div class="chat-container">
+      <div v-for="(message, index) in conversation" :key="index" :class="message.role">
+        <div class="message-bubble">{{ message.content }}</div>
+      </div>
+    </div>
+    <div class="input-container">
+      <input v-model="userInput" @keyup.enter="sendMessage" placeholder="Type your message..." />
+      <button @click="sendMessage">Send</button>
+    </div>
   </div>
 </template>
 
@@ -42,3 +49,71 @@ export default {
   },
 };
 </script>
+
+<style>
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+}
+
+.chat-container {
+  flex: 1;
+  padding: 20px;
+  overflow-y: auto;
+  display: flex;
+  flex-direction: column;
+}
+
+.message-bubble {
+  padding: 10px;
+  border-radius: 5px;
+  margin-bottom: 10px;
+  display: inline-block;
+}
+
+.user .message-bubble {
+  background-color: #1e88e5;
+  color: white;
+  align-self: flex-end;
+}
+
+.assistant .message-bubble {
+  background-color: #f1f1f1;
+  color: black;
+}
+
+.input-container {
+  display: flex;
+  padding: 20px;
+  background-color: #f7f7f7;
+  border-top: 1px solid #eee;
+}
+
+input {
+  flex: 1;
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  outline: none;
+}
+
+button {
+  margin-left: 10px;
+  padding: 10px;
+  background-color: #1e88e5;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+</style>
